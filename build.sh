40c86e90c3487d30647bd78a7bb3979d1acfb785
Bryan Richter <b@chreekat.net>
2025-12-04 16:14:50 +0200

    Add some devel helpers
    "Devel mode" mounts the work dir directly to the container and opens a
    shell there.
    
    Outside of devel mode, I also made it possible to target a particular
    package with `build.sh <target>`

--- build.sh
+++ build.sh
@@ -2,20 +2,34 @@
 
 set -e
 
+: ${DEVEL:=false}
+
 CONTAINER_ENGINE=$(which podman 2>/dev/null || which docker 2>/dev/null)
 if [[ -z "$CONTAINER_ENGINE" ]]; then
     echo "Please install Podman or Docker"
     exit 1
 fi
 
-$CONTAINER_ENGINE build -t p4lang-packages .
 
-mkdir -p build/
+if [[ "$DEVEL" != true ]]; then
+    $CONTAINER_ENGINE build -t p4lang-packages .
+    mkdir -p build/
 
-for target in pi bmv2 p4c; do
+    for target in pi bmv2 p4c; do
+        if [[ $# -gt 0 ]] && [[ "$target" != "$1" ]]; then
+            continue
+        fi
+        $CONTAINER_ENGINE run \
+            --rm \
+            --mount type=bind,source="$(pwd)/build",target=/mnt \
+            p4lang-packages \
+            bash -c "make ${target}-sdeb && dpkg -c *.deb && mv *.build *.buildinfo *.changes *.deb *.debian.tar.xz *.dsc *.orig.tar.gz /mnt";
+    done
+else
     $CONTAINER_ENGINE run \
-        --rm \
-        --mount type=bind,source="$(pwd)/build",target=/mnt \
+        -it \
+        --mount type=bind,source="$(pwd)",target=/mnt \
+        --workdir /mnt \
         p4lang-packages \
-        bash -c "make ${target}-sdeb && dpkg -c *.deb && mv *.build *.buildinfo *.changes *.deb *.debian.tar.xz *.dsc *.orig.tar.gz /mnt";
-done
+        bash
+fi
